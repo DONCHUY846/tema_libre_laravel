85e28e0bf848fe57f0c90b35e33c1a03d72945d4
Dungeon0Master <samuelosme05@gmail.com>
2025-03-01 12:52:58 -0600

    nose actulizacion

--- Recetas/routes/web.php
+++ Recetas/routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Login;
+use App\Http\Controllers\RegisterController;
+
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect('/login');
+
+ });
+
+//mostrar el formulario de login
+Route::get('/login', [Login::class, 'index'])->name('login');
+
+//ruta enviar el formulario
+Route::post('/login', [Login::class, 'login']);
+
+//cerrar sesion
+Route::post('/logout', [Login::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');;
+
+// Ruta para mostrar el formulario de registro
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+
+// Ruta para procesar el formulario de registro (POST)
+Route::post('/register', [RegisterController::class, 'register']);
